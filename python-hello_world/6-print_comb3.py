8033749f75d2ee4045ba0f9f57c2152b8872f573
Seemow <jebbor.mohamed@gmail.com>
2023-11-29 22:59:50 +0100

    Task 6 - 2nd Commit & Push

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for i in range(0,10):
     for j in range(i + 1, 10):
         if int(str(i)+str(j)) < 89:
-            print("{}".format(int(str(i)+str(j))) , end=", ")
+            print("{:02d}".format(int(str(i)+str(j))) , end=", ")
         else:
             print("{}".format(int(str(i)+str(j))))

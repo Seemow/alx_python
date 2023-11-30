c13849ab9b6577a41f242b9ad72dc05eaee6f142
Seemow <jebbor.mohamed@gmail.com>
2023-11-30 18:51:21 +0100

    Task 4 - Second Commit & Push

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,5 +1,10 @@
 def fibonacci_sequence(n):
+    if n <= 0:
+        return []  # Return an empty list for invalid input
+    
     fib_sequence = [0, 1]
+    
     while len(fib_sequence) < n:
         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-    return fib_sequence
+
+    return fib_sequence[:n]

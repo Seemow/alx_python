5bb676716059cd84ca0e1a4a74fc74018cb87334
Seemow <jebbor.mohamed@gmail.com>
2023-11-30 18:47:11 +0100

    Task 4 - 2nd Commit & Push

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,20 +1,5 @@
 def fibonacci_sequence(n):
-    if n <= 0:
-        return []  
-    elif n == 1:
-        return [0]  
-    elif n == 2:
-        return [0, 1] 
-
     fib_sequence = [0, 1]
-
-    # Generate the Fibonacci sequence up to the nth number
-    for i in range(2, n):
-        fib_sequence.append(fib_sequence[n-i])
-
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
     return fib_sequence
-
-# Example usage:
-n = 6
-result = fibonacci_sequence(n)
-print(result)

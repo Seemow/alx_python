2a0bad30d99d192be5328f29a99bf3f88262ea7c
Seemow <jebbor.mohamed@gmail.com>
2023-12-07 15:13:33 +0100

    Last commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print(" ")
-    else:
-        for i in range(0,len(matrix)):
-            for j in range(0, len(matrix[i])):
-                if j < len(matrix[i]) - 1:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-                else:
-                    print("{:d}".format(matrix[i][j]))
+    for i in range(0,len(matrix)):
+        for j in range(0, len(matrix[i])):
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]))

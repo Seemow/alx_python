853d9d5f9e963b187ad8153551a3697c5b9576da
Seemow <jebbor.mohamed@gmail.com>
2023-12-07 14:35:52 +0100

    Task 0

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     my_list = []
-    for i in range(0, len(my_string) - 1):
+    for i in range(0, len(my_string)):
         if my_string[i] != "c" and my_string[i] != "C":
             my_list.append(my_string[i])
     return ''.join(my_list)

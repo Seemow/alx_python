c00a8bc457f6c595be9c09681cdcf377b3787651
Seemow <jebbor.mohamed@gmail.com>
2023-12-07 11:41:00 +0100

    Task 1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
+from add_0 import add
+
 a = 1
 b = 2 
-
-from add_0 import add
 
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+def print_arguments():
+    num_arguments = len(argv) - 1 
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    elif num_arguments > 1:
+        print("{} arguments:".format(num_arguments))
+        for i in range(1, num_arguments + 1):
+            print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    print_arguments()

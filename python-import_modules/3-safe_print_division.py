8a7e02a4631375f480b32d23d009d5634d9d990e
Seemow <jebbor.mohamed@gmail.com>
2023-12-07 13:21:36 +0100

    Task 3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,6 +5,4 @@ def safe_print_division(a, b):
         division = None
     finally:
         print("Inside result: {}".format(division))
-        print("{:d} / {:d} = {}".format(a, b, division))
-        pass
-        
+        return division

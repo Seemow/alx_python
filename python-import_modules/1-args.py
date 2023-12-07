860e4444876e23bb12fcb2ddef3a0f2454ecd342
Seemow <jebbor.mohamed@gmail.com>
2023-12-07 12:04:51 +0100

    Task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,15 @@
-from sys import argv
-
-num_arguments = len(argv) - 1 
-
-if num_arguments == 0:
-    print("0 arguments.")
-elif num_arguments == 1:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-elif num_arguments > 1:
-    print("{} arguments:".format(num_arguments))
-    for i in range(1, num_arguments + 1):
-        print("{}: {}".format(i, argv[i]))
 
 if __name__ == "__main__":
-    pass
+    from sys import argv
+
+    num_arguments = len(argv) - 1 
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    elif num_arguments > 1:
+        print("{} arguments:".format(num_arguments))
+        for i in range(1, num_arguments + 1):
+            print("{}: {}".format(i, argv[i]))

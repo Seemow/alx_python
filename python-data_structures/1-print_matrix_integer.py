68abfd58bae9134e2d5626cf05acccac7c8a2229
Seemow <jebbor.mohamed@gmail.com>
2023-12-07 15:02:40 +0100

    Task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0,len(matrix)):
-        for j in range(0, len(matrix[i])):
-            if j < len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end=" ")
-            else:
-                print("{:d}".format(matrix[i][j]))
+    if len(matrix) == 0:
+        print()
+    else:
+        for i in range(0,len(matrix)):
+            for j in range(0, len(matrix[i])):
+                if j < len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+                else:
+                    print("{:d}".format(matrix[i][j]))
